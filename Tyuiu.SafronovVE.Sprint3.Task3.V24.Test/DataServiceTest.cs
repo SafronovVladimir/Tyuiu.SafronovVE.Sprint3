332d4f1bafa574718a7c9d52bb8cfd808bc654c7
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-09-18 18:50:25 +0500

    Task3

---
+++ Tyuiu.SafronovVE.Sprint3.Task3.V24.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.SafronovVE.Sprint3.Task3.V24.Lib;
+namespace Tyuiu.SafronovVE.Sprint3.Task3.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidReplaceCharInString()
+        {
+            DataService ds = new DataService();
+            string value;
+            char replaceable, replacement;
+            value = "gft hggt ntg";
+            replaceable = 'g';
+            replacement = '*';
+            var res = ds.ReplaceCharInString(value, replaceable, replacement);
+            var wait = "*ft h**t nt*";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SafronovVE.Sprint3.Task3.V24/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.SafronovVE.Sprint3.Task3.V24.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Сафронов В. Е. | ИБКСб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #24                                                             *");
+        Console.WriteLine("* Выполнил: Сафронов Владимир Евгеньевич | ИБКСб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Используя цикл foreach заменить буквы g  на * в строке: gft hggt ntg    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string value = "gft hggt ntg";
+        char replaceable = 'g';
+        char replacement = '*';
+
+        Console.WriteLine("Исходная строка = " + value);
+        Console.WriteLine("Искомый символ = " + replaceable);
+        Console.WriteLine("Символ для замены = " +  replacement);
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Итоговая строка: " + ds.ReplaceCharInString(value, replaceable, replacement));
+
+        Console.ReadKey();
+    }
+}
